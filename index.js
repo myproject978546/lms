a8c7369f6e4d322e91fdc3297af330a76e266e5d
nitesh <niteshkymar1@gmail.com>
2023-07-25 09:02:33 +0530

    try to create controller

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const user = require('./models/user')
-const courses = require('./models/courses')
+// const courses = require('./models/courses')
 const dbconnect = require('./models/dbconnect')
 const cookieParser = require('cookie-parser')
 const { verifytoken , generatetoken } = require('./auth/index')
